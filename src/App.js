7cf883eeb1afb25ce1d070a0aa34b02d6c581aba
jmihelson <jmihelson@gmail.com>
2022-05-03 19:08:28 +0300

    nimed

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="app">
       <Tweet className="Jüri Mihelson" message="Tänane ilm tuleb ilus." likes="189"/>
-      <Tweet className="Marlen Lillenberk" message="Homme hakkab sadama." likes="278"/>
-      <Tweet className="Larissa Rump" message="Väga pilvine on." likes="65"/>
-      <Tweet className="Heino Rump" message="Ilus suvine on väljas." likes="29"/>
+      <Tweet className="John Travolta" message="Homme hakkab sadama." likes="278"/>
+      <Tweet className="Kelly Preston" message="Väga pilvine on." likes="65"/>
+      <Tweet className="Samuel L. Jackson" message="Ilus suvine on väljas." likes="29"/>
     </div>
   );
 }
